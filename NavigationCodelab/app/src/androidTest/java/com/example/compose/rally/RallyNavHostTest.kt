8bb2dcc6e368fa2c3ff7c1d175aa8a67cd6c7e20
shidobecker <juliowebdeveloper@hotmail.com>
2021-12-24 17:21:15 -0300

    Finished navigation

---
+++ NavigationCodelab/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -0,0 +1,46 @@
+package com.example.compose.rally
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class RallyNavHostTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    lateinit var navController: NavHostController
+
+    @Before
+    fun setupRallyNavHost() {
+        composeTestRule.setContent {
+            navController = rememberNavController()
+            RallyNavHost(navController = navController, PaddingValues(16.dp))
+        }
+    }
+
+    @Test
+    fun rallyNavHost(){
+        composeTestRule
+            .onNodeWithContentDescription("Overview Screen")
+            .assertIsDisplayed()
+
+    }
+
+    @Test
+    fun rallyNavHost_navigateToAllAccounts_viaUI() {
+        composeTestRule
+            .onNodeWithContentDescription("All Accounts")
+            .performClick()
+        composeTestRule
+            .onNodeWithContentDescription("Accounts Screen")
+            .assertIsDisplayed()
+    }
+}
